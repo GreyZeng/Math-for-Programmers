ff68b5937ca7bea088728e781c0efbe8592a0edc
GreyZeng <410486047@qq.com>
2023-11-15 16:25:50 +0800

    update code

--- phone_number_check.py
+++ phone_number_check.py
@@ -12,3 +12,9 @@ print(mo.groups())
 areaCode, mainNumber = mo.groups()
 print(areaCode)
 print(mainNumber)
+
+phoneNumRegex = re.compile(r'(\(\d\d\d\)) (\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search('My phone number is (415) 333-3323.')
+
+print(mo.group(1))
+print(mo.group(2))
