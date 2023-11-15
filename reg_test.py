ecbc2922bbc0420f81a17b19738d275d88d09d8b
GreyZeng <410486047@qq.com>
2023-11-15 16:28:31 +0800

    update code

--- reg_test.py
+++ reg_test.py
@@ -18,3 +18,11 @@ mo = phoneNumRegex.search('My phone number is (415) 333-3323.')
 
 print(mo.group(1))
 print(mo.group(2))
+
+
+# 使用管道匹配多个分组
+hero_regex = re.compile(r'Batman|Tina Fey')
+mo1 = hero_regex.search('Batman and Tina Fey.')
+print(mo1.group())
+mo1 = hero_regex.search(' Tina Fey and Batman.')
+print(mo1.group())
