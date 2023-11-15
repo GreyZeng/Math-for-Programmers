8f854f080cb0cda0b49a73bb9c74c628ff1ec879
GreyZeng <410486047@qq.com>
2023-11-15 16:14:41 +0800

    update code

--- phone_number_check.py
+++ phone_number_check.py
@@ -3,3 +3,8 @@ import re
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 mo = phoneNumRegex.search('My number is 415-555-4235')
 print(mo.group())
+
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search('My number is 415-555-4235')
+print(mo.group(1))
+print(mo.group(2))
