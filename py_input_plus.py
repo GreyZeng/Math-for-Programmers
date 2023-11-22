dd9f493688b19fd2f90c4ab517f19e5e48a86c5e
GreyZeng <410486047@qq.com>
2023-11-22 16:50:36 +0800

    update code

--- py_input_plus.py
+++ py_input_plus.py
@@ -1,3 +1,15 @@
 import pyinputplus as pyip
 
-response = pyip.inputNum()
+# response = pyip.inputNum()
+# response = pyip.inputInt(prompt='Enter a number: ')
+# response = pyip.inputNum(prompt='Enter a number: ', min=4)
+# response = pyip.inputNum(prompt='Enter a number: ', greaterThan=4)
+# response = pyip.inputNum('>', min=4, lessThan=6)
+# response = pyip.inputNum('>', min=4, lessThan=6)
+# response = pyip.inputNum(blank=True)
+# 两次输入错误就报错
+# response = pyip.inputNum(limit=2)
+# 10 秒等待还没输入成功就报错
+# response = pyip.inputNum(timeout=10)
+# 不会引发 RetryLimitException, 只会返回字符串 N/A
+response = pyip.inputNum(limit=2, default='N/A')
