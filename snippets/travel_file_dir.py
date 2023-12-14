69bc22d6bbc959dc25b0f08761aa9ef2abd32d2d
GreyZeng <410486047@qq.com>
2023-12-14 15:18:46 +0800

    add notes

--- snippets/travel_file_dir.py
+++ snippets/travel_file_dir.py
@@ -2,10 +2,14 @@
 import os
 from pathlib import Path
 
+# os.walk 返回三个值
+# 当前文件夹名称的字符串
+# 当前文件夹中子文件夹的字符串的列表
+# 当前文件夹中文件的字符串列表
 for folder_name, sub_folders, file_names in os.walk(Path.home()):
     print('The Current folder is ' + folder_name)
     for sub_folder in sub_folders:
-        print('SUBFOLDER OF ' + folder_name + ": " + sub_folder)
+        print('SUB FOLDER OF ' + folder_name + ": " + sub_folder)
     for file_name in file_names:
         print('FILE INSIDE ' + folder_name + ": " + file_name)
     print(' ')
